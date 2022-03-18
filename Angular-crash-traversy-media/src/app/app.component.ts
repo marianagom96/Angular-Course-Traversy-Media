3f5571d8e5aed155b6002512c2a3402f5576b3d1
marianagom <marianagom96@gmail.com>
2022-03-17 22:43:50 -0500

    begining

---
+++ Angular-crash-traversy-media/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+//Declaration Component selector is the HTML tag that's going to be used to embed the component (since it's the root component it is embeded int the index.html) The ones created by the coder will be embeded to the app.component.html
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+//properties of the component cutom life cycle methods
+export class AppComponent {
+  title: string = 'Task Tracker';
+}
